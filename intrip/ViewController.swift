ca4a52c259aee24e9760d57f9db7a967552cac70
Gilles David <gdvd@me.com>
2021-12-17 19:13:44 +0100

    Initial Commit

---
+++ intrip/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  intrip
+//
+//  Created by Gilles David on 17/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
